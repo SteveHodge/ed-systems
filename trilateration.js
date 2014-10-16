0b579722116a82a3b0029c6aa527e08b77262b29
Steve Hodge <stevehodge@gmail.com>
2014-10-17 02:08:46 +1300

    Switched to storing floating point coordinates in system.json (Ly units). Added SQL output to entry.html and made SQL/JSON output update immediately on recalculation. Updated table generation code in several pages. Added bulklocate.html and distance.json crowdsourced data.

--- trilateration.js
+++ trilateration.js
@@ -1,4 +1,4 @@
-// dists is an array of reference objects (properties x, y, z, dist)
+// dists is an array of reference objects (properties x, y, z, distance)
 // returns an object containing the best candidate found (properties x, y, z, totalSqErr, i1, i2, i3)
 // i1, i2, i3 are the indexes into dists[] that were reference points for the candidate
 // totalSqErr is the total of the squares of the difference between the supplied distance and the calculated distance to each system in dists[]
@@ -18,9 +18,9 @@ function getBestCandidate(dists) {
 					candidates[1].totalSqErr = 0;
 					
 		 			for (i4 = 0; i4 < dists.length; i4++) {
-						var err = Math.abs(dist(candidates[0], dists[i4]) - dists[i4].dist);
+						var err = Math.abs(dist(candidates[0], dists[i4]) - dists[i4].distance);
 		 				candidates[0].totalSqErr += err*err;
-		 				err = Math.abs(dist(candidates[1], dists[i4]) - dists[i4].dist);
+		 				err = Math.abs(dist(candidates[1], dists[i4]) - dists[i4].distance);
 		 				candidates[1].totalSqErr += err*err;
 		 			}
 					if (bestCandidate === null || bestCandidate.totalSqErr > candidates[0].totalSqErr) {
@@ -44,7 +44,7 @@ function getBestCandidate(dists) {
 	return bestCandidate;
 }
 
-// dists is an array of reference objects (properties x, y, z, dist)
+// dists is an array of reference objects (properties x, y, z, distance)
 // i1, i2, i3 indexes of the references to use to calculate the candidates
 // returns an array of two points (properties x, y, z). if the supplied reference points are disjoint then an empty array is returned
 function getCandidates(dists, i1, i2, i3) {
@@ -61,9 +61,9 @@ function getCandidates(dists, i1, i2, i3) {
 	ey = scalarProd( 1/length(ey), ey);
 	var j = dotProd(ey, diff(p3, p1));
 
-	var x = (p1.dist*p1.dist - p2.dist*p2.dist + d*d) / (2*d);
-	var y = ((p1.dist*p1.dist - p3.dist*p3.dist + i*i + j*j) / (2*j)) - (i*x/j);
-	var zsq = p1.dist*p1.dist - x*x - y*y;
+	var x = (p1.distance*p1.distance - p2.distance*p2.distance + d*d) / (2*d);
+	var y = ((p1.distance*p1.distance - p3.distance*p3.distance + i*i + j*j) / (2*j)) - (i*x/j);
+	var zsq = p1.distance*p1.distance - x*x - y*y;
 	if (zsq < 0) {
 		//console.log("inconsistent distances (z^2 = "+zsq+")");
 		return [];
@@ -73,16 +73,28 @@ function getCandidates(dists, i1, i2, i3) {
 		var coord1 = sum(sum(p1,scalarProd(x,ex)),scalarProd(y,ey));
 		var coord2 = diff(coord1,scalarProd(z,ez));
 		coord1 = sum(coord1,scalarProd(z,ez));
-		//console.log("ex = "+vectorToString(ex)+", ey = "+vectorToString(ey)+", ez = "+vectorToString(ez));
-		//console.log("candidate 1: ("+(coord1.x/32.0)+", "+(coord1.y/32.0)+", "+(coord1.z/32.0)+")");
-		//console.log("candidate 2: ("+(coord2.x/32.0)+", "+(coord2.y/32.0)+", "+(coord2.z/32.0)+")");
-		
 		return [coord1, coord2];
 	}
 }
 
+// returns a vector with the components of v rounded to 1/32
+function gridLocation(v) {
+	return {
+		x: (Math.round(v.x*32)/32),
+		y: (Math.round(v.y*32)/32),
+		z: (Math.round(v.z*32)/32)
+	};
+}
+
 function vectorToString(v) {
 	return "("+v.x+", "+v.y+", "+v.z+")";
+}
+
+// copies the vector components (x, y, z properties) to object o
+function setVector(o, v) {
+	o.x = v.x;
+	o.y = v.y;
+	o.z = v.z;
 }
 
 // p1 and p2 are objects that have x, y, and z properties
@@ -144,17 +156,17 @@ function sum(p1, p2) {
 	};
 }
 
-// dists is an array of four reference objects (properties x, y, z, dist)
+// dists is an array of four reference objects (properties x, y, z, distance)
 // returns coordinate object (properties x, y, z)
 function tunamageCoords(dists) {
 	var b = diff(dists[1], dists[0]);	// 2nd system relative to 1st system
 	var c = diff(dists[2], dists[0]);	// 3rd system relative to 1st system
 	var d = diff(dists[3], dists[0]);	// 4th system relative to 1st system
 
-	var ea = dists[0].dist*dists[0].dist;
-	var eb = dists[1].dist*dists[1].dist;
-	var ec = dists[2].dist*dists[2].dist;
-	var ed = dists[3].dist*dists[3].dist;
+	var ea = dists[0].distance*dists[0].distance;
+	var eb = dists[1].distance*dists[1].distance;
+	var ec = dists[2].distance*dists[2].distance;
+	var ed = dists[3].distance*dists[3].distance;
 
 	var p = (ea-eb+b.x*b.x+b.y*b.y+b.z*b.z)/2;
 	var q = (ea-ec+c.x*c.x+c.y*c.y+c.z*c.z)/2;
@@ -176,7 +188,7 @@ function updateSortArrow(event, data) {
 // data.direction - the sorting direction (either asc or desc)
 	var th = $(this).find("th");
 	th.find(".arrow").remove();
-	var arrow = data.direction === "asc" ? "?" : "?";
+	var arrow = data.direction === "asc" ? "\u2191" : "\u2193";
 	th.eq(data.column).append('<span class="arrow">' + arrow +'</span>');
 }
 
@@ -185,4 +197,18 @@ function sortOptionalFloat(a,b) {
 	if (isNaN(parseFloat(a))) return 1;
 	if (isNaN(parseFloat(b))) return -1;
 	return parseFloat(a)-parseFloat(b);
+}
+
+// sort function that treats missing value (and values that can't be parsed as integers) as the largest values
+function sortOptionalInt(a,b) {
+	if (isNaN(parseInt(a))) return 1;
+	if (isNaN(parseInt(b))) return -1;
+	return parseInt(a)-parseInt(b);
+}
+
+// returns a string containing an sql insert statement for TradeDangerous
+function getSQL(s) {
+	var quotedName = s.name.replace("'","''");
+	var d = (new Date()).toISOString().replace('T',' ').substr(0,19);
+	return "INSERT INTO \"System\" VALUES(,'"+quotedName+"',"+s.x+","+s.y+","+s.z+",'"+d+"');\n";
 }
